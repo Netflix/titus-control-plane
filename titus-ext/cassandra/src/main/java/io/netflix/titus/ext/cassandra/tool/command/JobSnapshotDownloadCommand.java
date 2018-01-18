1c017c374c5a6919e6ddf295500c9a271539591b
Tomasz Bak <tbak@netflix.com>
2018-01-17 16:45:48 -0800

    Add Cassandra job snapshot commands

---
+++ titus-ext/cassandra/src/main/java/io/netflix/titus/ext/cassandra/tool/command/JobSnapshotDownloadCommand.java
@@ -0,0 +1,42 @@
+package io.netflix.titus.ext.cassandra.tool.command;
+
+import java.io.File;
+
+import io.netflix.titus.ext.cassandra.tool.Command;
+import io.netflix.titus.ext.cassandra.tool.CommandContext;
+import io.netflix.titus.ext.cassandra.tool.snapshot.JobSnapshotDownloader;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+public class JobSnapshotDownloadCommand implements Command {
+
+    @Override
+    public String getDescription() {
+        return "Save job tables content to a disk in JSON format";
+    }
+
+    @Override
+    public CommandType getCommandType() {
+        return CommandType.TargetKeySpace;
+    }
+
+    @Override
+    public Options getOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder("o")
+                .longOpt("output folder")
+                .required()
+                .hasArg()
+                .desc("Output folder for snapshot files")
+                .build()
+        );
+        return options;
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        File output = new File(context.getCommandLine().getOptionValue('o'));
+        JobSnapshotDownloader downloader = new JobSnapshotDownloader(context.getTargetSession(), output);
+        downloader.download();
+    }
+}
