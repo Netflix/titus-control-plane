Netflix/titus-control-plane

commit 5a4619c96389e653902cda1fbd2ae292c3c2893f

Do System.exit if the activation phase fails (#193)
* Do System.exit if the activation phase fails
* Better time tracking for the leader activation phase
* Use polling not passive gauges, as the latter expire

titus-server-master/src/main/java/io/netflix/titus/master/cluster/DefaultLeaderActivator.java
@@ -16,18 +16,22 @@
 
 package io.netflix.titus.master.cluster;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import com.google.inject.Injector;
 import com.netflix.spectator.api.Registry;
+import com.netflix.spectator.api.patterns.PolledMeter;
+import io.netflix.titus.common.framework.fit.FitFramework;
+import io.netflix.titus.common.framework.fit.FitInjection;
+import io.netflix.titus.common.runtime.TitusRuntime;
 import io.netflix.titus.common.util.guice.ActivationLifecycle;
 import io.netflix.titus.common.util.guice.ContainerEventBus;
 import io.netflix.titus.common.util.guice.ContainerEventBus.ContainerEventListener;
 import io.netflix.titus.common.util.guice.ContainerEventBus.ContainerStartedEvent;
+import io.netflix.titus.common.util.time.Clock;
 import io.netflix.titus.master.MetricConstants;
 import io.netflix.titus.master.scheduler.SchedulingService;
 import org.slf4j.Logger;
@@ -50,43 +54,85 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
     private final AtomicReference<State> stateRef = new AtomicReference<>(State.Starting);
 
     private final Injector injector;
+    private final Clock clock;
     private final ActivationLifecycle activationLifecycle;
 
-    private final AtomicInteger isLeaderGauge;
-    private final AtomicInteger isActivatedGauge;
-    private final AtomicLong activationTimeGauge;
-
+    private volatile boolean leader;
     private volatile boolean activated;
-    private volatile long electionTime = -1;
+
+    private volatile long electionTimestamp = -1;
+    private volatile long activationStartTimestamp = -1;
+    private volatile long activationEndTimestamp = -1;
     private volatile long activationTime = -1;
+
+    private final Optional<FitInjection> beforeActivationFitInjection;
 
     @Inject
     public DefaultLeaderActivator(Injector injector,
                                   ContainerEventBus eventBus,
                                   ActivationLifecycle activationLifecycle,
-                                  Registry registry) {
+                                  TitusRuntime titusRuntime) {
         this.injector = injector;
         this.activationLifecycle = activationLifecycle;
-        this.isLeaderGauge = registry.gauge(MetricConstants.METRIC_LEADER + "isLeaderGauge", new AtomicInteger());
-        this.isActivatedGauge = registry.gauge(MetricConstants.METRIC_LEADER + "isActivatedGauge", new AtomicInteger());
-        this.activationTimeGauge = registry.gauge(MetricConstants.METRIC_LEADER + "activationTime", new AtomicLong());
+        this.clock = titusRuntime.getClock();
+
+        Registry registry = titusRuntime.getRegistry();
+
+        PolledMeter.using(registry)
+                .withName(MetricConstants.METRIC_LEADER + "isLeaderGauge")
+                .monitorValue(this, self -> leader ? 1 : 0);
+
+        PolledMeter.using(registry)
+                .withName(MetricConstants.METRIC_LEADER + "isActivatedGauge")
+                .monitorValue(this, self -> activated ? 1 : 0);
+
+        PolledMeter.using(registry)
+                .withName(MetricConstants.METRIC_LEADER + "activationTime")
+                .monitorValue(this, self -> getActivationTime());
+
+        PolledMeter.using(registry)
+                .withName(MetricConstants.METRIC_LEADER + "inActiveStateTime")
+                .monitorValue(this, self -> isActivated() ? clock.wallTime() - activationEndTimestamp : 0L);
+
+        FitFramework fit = titusRuntime.getFitFramework();
+        if (fit.isActive()) {
+            FitInjection beforeActivationFitInjection = fit.newFitInjectionBuilder("beforeActivation")
+                    .withDescription("Inject failures after the node becomes the leader, but before the activation process is started")
+                    .build();
+            fit.getRootComponent().getChild(COMPONENT).addInjection(beforeActivationFitInjection);
+
+            this.beforeActivationFitInjection = Optional.of(beforeActivationFitInjection);
+        } else {
+            this.beforeActivationFitInjection = Optional.empty();
+        }
+
         eventBus.registerListener(this);
     }
 
     @Override
-    public long getElectionTime() {
-        return electionTime;
+    public long getElectionTimestamp() {
+        return electionTimestamp;
+    }
+
+    @Override
+    public long getActivationEndTimestamp() {
+        return activationEndTimestamp;
     }
 
     @Override
     public long getActivationTime() {
-        return activationTime;
+        if (isActivated()) {
+            return activationTime;
+        }
+        if (!isLeader()) {
+            return -1;
+        }
+        return clock.wallTime() - activationStartTimestamp;
     }
 
     @Override
     public boolean isLeader() {
-        State state = stateRef.get();
-        return state == State.Leader || state == State.StartedLeader;
+        return leader;
     }
 
     @Override
@@ -98,13 +144,13 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
     public void becomeLeader() {
         logger.info("Becoming leader now");
         if (stateRef.compareAndSet(State.Starting, State.Leader)) {
-            isLeaderGauge.set(1);
-            electionTime = System.currentTimeMillis();
+            leader = true;
+            electionTimestamp = clock.wallTime();
             return;
         }
         if (stateRef.compareAndSet(State.Started, State.StartedLeader)) {
-            isLeaderGauge.set(1);
-            electionTime = System.currentTimeMillis();
+            leader = true;
+            electionTimestamp = clock.wallTime();
             activate();
         }
         logger.warn("Unexpected to be told to enter leader mode more than once, ignoring.");
@@ -113,8 +159,8 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
     @Override
     public void stopBeingLeader() {
         logger.info("Asked to stop being leader now");
-        isLeaderGauge.set(0);
-        isActivatedGauge.set(0);
+        leader = false;
+        activated = false;
 
         if (!isLeader()) {
             logger.warn("Unexpected to be told to stop being leader when we haven't entered leader mode before, ignoring.");
@@ -141,11 +187,26 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
     }
 
     private void activate() {
-        activationLifecycle.activate();
-        injector.getInstance(SchedulingService.class).startScheduling();
-        isActivatedGauge.set(1);
-        activated = true;
-        activationTime = System.currentTimeMillis();
-        activationTimeGauge.set(activationLifecycle.getActivationTimeMs());
+        this.activationStartTimestamp = clock.wallTime();
+
+        beforeActivationFitInjection.ifPresent(i -> i.beforeImmediate("beforeActivation"));
+
+        try {
+            try {
+                activationLifecycle.activate();
+                injector.getInstance(SchedulingService.class).startScheduling();
+            } catch (Exception e) {
+                stopBeingLeader();
+
+                // As stopBeingLeader method not always terminates the process, lets make sure it does.
+                System.exit(-1);
+            }
+        } catch (Throwable e) {
+            System.exit(-1);
+        }
+
+        this.activated = true;
+        this.activationEndTimestamp = clock.wallTime();
+        this.activationTime = activationEndTimestamp - activationStartTimestamp;
     }
 }

titus-server-master/src/main/java/io/netflix/titus/master/TitusRuntimeModule.java
@@ -42,6 +42,7 @@ import io.netflix.titus.common.runtime.internal.DefaultTitusRuntime;
 import io.netflix.titus.common.util.guice.ContainerEventBusModule;
 import io.netflix.titus.common.util.rx.eventbus.RxEventBus;
 import io.netflix.titus.common.util.rx.eventbus.internal.DefaultRxEventBus;
+import io.netflix.titus.master.cluster.LeaderActivator;
 import io.netflix.titus.master.mesos.MesosStatusOverrideFitAction;
 import io.netflix.titus.master.scheduler.SchedulingService;
 import org.slf4j.Logger;
@@ -85,6 +86,7 @@ public class TitusRuntimeModule extends AbstractModule {
 
         if (fitFramework.isActive()) {
             FitComponent root = fitFramework.getRootComponent();
+            root.createChild(LeaderActivator.COMPONENT);
             root.createChild(V3JobOperations.COMPONENT);
             root.createChild(SchedulingService.COMPONENT);
             root.createChild(VirtualMachineMasterService.COMPONENT);

titus-server-master/src/main/java/io/netflix/titus/master/cluster/LeaderActivator.java
@@ -20,7 +20,12 @@ package io.netflix.titus.master.cluster;
  * Counterpart of {@link DefaultLeaderElector} that performs lead activation/inactivation mechanics.
  */
 public interface LeaderActivator {
-    long getElectionTime();
+
+    String COMPONENT = "clusterManager";
+
+    long getElectionTimestamp();
+
+    long getActivationEndTimestamp();
 
     long getActivationTime();
 

titus-server-master/src/main/java/io/netflix/titus/master/endpoint/v2/rest/ServerStatusResource.java
@@ -74,7 +74,7 @@ public class ServerStatusResource {
             );
         }
 
-        boolean active = activationLifecycle.getActivationTimeMs() != -1;
+        boolean active = leaderActivator.isActivated();
 
         List<ServerStatusRepresentation.ServiceActivation> serviceActivationTimes = activationLifecycle.getServiceActionTimesMs().stream()
                 .sorted((first, second) -> Long.compare(second.getRight(), first.getRight()))
@@ -89,9 +89,9 @@ public class ServerStatusResource {
                 true,
                 active,
                 DateTimeExt.toTimeUnitString(uptime),
-                DateTimeExt.toUtcDateTimeString(leaderActivator.getElectionTime()),
-                DateTimeExt.toUtcDateTimeString(leaderActivator.getActivationTime()),
-                active ? DateTimeExt.toTimeUnitString(activationLifecycle.getActivationTimeMs()) : NOT_APPLICABLE,
+                DateTimeExt.toUtcDateTimeString(leaderActivator.getElectionTimestamp()),
+                DateTimeExt.toUtcDateTimeString(leaderActivator.getActivationEndTimestamp()),
+                active ? DateTimeExt.toTimeUnitString(leaderActivator.getActivationTime()) : NOT_APPLICABLE,
                 serviceActivationTimes,
                 serviceActivationOrder
         );

titus-server-master/src/test/java/io/netflix/titus/master/endpoint/v2/rest/ServerStatusResourceTest.java
@@ -66,14 +66,12 @@ public class ServerStatusResourceTest {
     public void testActivatedServer() throws Exception {
         when(leaderActivator.isLeader()).thenReturn(true);
         when(leaderActivator.isActivated()).thenReturn(true);
-        when(activationLifecycle.getActivationTimeMs()).thenReturn(100L);
         when(activationLifecycle.getServiceActionTimesMs()).thenReturn(Collections.singletonList(Pair.of("myService", 90L)));
 
         ServerStatusRepresentation result = client.doGET(ServerStatusResource.PATH_API_V2_STATUS, ServerStatusRepresentation.class);
 
         assertThat(result.isLeader()).isTrue();
         assertThat(result.isActive()).isTrue();
-        assertThat(result.getActivationTime()).isEqualTo("100ms");
         assertThat(result.getServiceActivationTimes()).hasSize(1);
         assertThat(result.getServiceActivationTimes().get(0).getDuration()).isEqualTo("90ms");
         assertThat(result.getServiceActivationOrder()).hasSize(1);

commit a256e6071031001fcf8563c51b7de7ffcf89ac97

Do System.exit if no longer leader
The bug was introduced in change #193

titus-server-master/src/main/java/io/netflix/titus/master/cluster/DefaultLeaderActivator.java
@@ -152,6 +152,7 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
             leader = true;
             electionTimestamp = clock.wallTime();
             activate();
+            return;
         }
         logger.warn("Unexpected to be told to enter leader mode more than once, ignoring.");
     }
@@ -159,13 +160,14 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
     @Override
     public void stopBeingLeader() {
         logger.info("Asked to stop being leader now");
-        leader = false;
-        activated = false;
 
-        if (!isLeader()) {
+        if (!leader) {
             logger.warn("Unexpected to be told to stop being leader when we haven't entered leader mode before, ignoring.");
             return;
         }
+
+        leader = false;
+        activated = false;
 
         // Various services may have built in-memory state that is currently not easy to revert to initialization state.
         // Until we create such a lifecycle feature for each service and all of their references, best thing to do is to
@@ -182,6 +184,7 @@ public class DefaultLeaderActivator implements LeaderActivator, ContainerEventLi
         }
         if (stateRef.compareAndSet(State.Leader, State.StartedLeader)) {
             activate();
+            return;
         }
         logger.warn("ContainerStartedEvent received while in state {}; ignoring", stateRef.get());
     }