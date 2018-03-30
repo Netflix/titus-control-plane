79d0830e5043e0f79b24cbe5260312fb639cd735
Amit Joshi <ajoshi@netflix.com>
2018-03-30 12:12:42 -0700

    AutoScalingResource REST

--- titus-server-federation/src/test/java/io/netflix/titus/federation/service/AggregatingAutoScalingServiceTest.java
+++ titus-server-federation/src/test/java/io/netflix/titus/federation/service/AggregatingAutoScalingServiceTest.java
@@ -15,20 +15,13 @@
  */
 package io.netflix.titus.federation.service;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import com.google.protobuf.DoubleValue;
-import com.google.protobuf.Empty;
 import com.netflix.titus.grpc.protogen.DeletePolicyRequest;
 import com.netflix.titus.grpc.protogen.GetPolicyResult;
-import com.netflix.titus.grpc.protogen.Job;
 import com.netflix.titus.grpc.protogen.JobId;
 import com.netflix.titus.grpc.protogen.PutPolicyRequest;
 import com.netflix.titus.grpc.protogen.ScalingPolicy;
@@ -36,55 +29,14 @@ import com.netflix.titus.grpc.protogen.ScalingPolicyID;
 import com.netflix.titus.grpc.protogen.ScalingPolicyResult;
 import com.netflix.titus.grpc.protogen.TargetTrackingPolicyDescriptor;
 import com.netflix.titus.grpc.protogen.UpdatePolicyRequest;
-import io.grpc.ManagedChannel;
-import io.grpc.Status;
-import io.grpc.StatusException;
 import io.grpc.StatusRuntimeException;
-import io.grpc.stub.StreamObserver;
-import io.grpc.testing.GrpcServerRule;
-import io.netflix.titus.api.federation.model.Cell;
-import io.netflix.titus.common.grpc.AnonymousSessionContext;
-import io.netflix.titus.federation.startup.GrpcConfiguration;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import rx.observers.AssertableSubscriber;
 
-import static com.netflix.titus.grpc.protogen.AutoScalingServiceGrpc.AutoScalingServiceImplBase;
-import static com.netflix.titus.grpc.protogen.JobManagementServiceGrpc.JobManagementServiceImplBase;
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
-public class AggregatingAutoScalingServiceTest {
-    static final String POLICY_1 = "policy1";
-    static final String POLICY_2 = "policy2";
-    static final String JOB_1 = "job1";
-    static final String JOB_2 = "job2";
-
-    @Rule
-    public final GrpcServerRule cellOne = new GrpcServerRule().directExecutor();
-
-
-    @Rule
-    public final GrpcServerRule cellTwo = new GrpcServerRule().directExecutor();
-
-    private AggregatingAutoScalingService service;
-
-    @Before
-    public void setUp() {
-        CellConnector connector = mock(CellConnector.class);
-        Map<Cell, ManagedChannel> cellMap = new HashMap<>();
-        cellMap.put(new Cell("one", "1"), cellOne.getChannel());
-        cellMap.put(new Cell("two", "2"), cellTwo.getChannel());
-        when(connector.getChannels()).thenReturn(cellMap);
-
-        GrpcConfiguration grpcConfiguration = mock(GrpcConfiguration.class);
-        when(grpcConfiguration.getRequestTimeoutMs()).thenReturn(1000L);
-        service = new AggregatingAutoScalingService(connector, new AnonymousSessionContext(), grpcConfiguration);
-    }
-
+public class AggregatingAutoScalingServiceTest extends AggregatingAutoScalingTestBase {
 
     @Test
     public void getPoliciesFromTwoCells() {
@@ -252,7 +204,6 @@ public class AggregatingAutoScalingServiceTest {
         assertThat(onNextEvents1.get(0).getItems(1).getJobId()).isEqualTo(JOB_2);
 
         DeletePolicyRequest deletePolicyRequest = DeletePolicyRequest.newBuilder().setId(ScalingPolicyID.newBuilder().setId(newPolicyId).build()).build();
-
         AssertableSubscriber<Void> testSubscriber3 = service.deleteAutoScalingPolicy(deletePolicyRequest).test();
         testSubscriber3.assertNoErrors();
 
@@ -299,102 +250,5 @@ public class AggregatingAutoScalingServiceTest {
         assertThat(onNextEvents.size()).isEqualTo(1);
         assertThat(onNextEvents.get(0).getItemsCount()).isEqualTo(0);
 
-    }
-
-
-    private static class CellWithJobs extends JobManagementServiceImplBase {
-        private List<String> jobIds;
-
-        CellWithJobs(List<String> jobIds) {
-            this.jobIds = jobIds;
-        }
-
-        @Override
-        public void findJob(JobId request, StreamObserver<Job> responseObserver) {
-            if (jobIds.contains(request.getId())) {
-                responseObserver.onNext(Job.newBuilder().setId(request.getId()).build());
-                responseObserver.onCompleted();
-            } else {
-                responseObserver.onError(Status.NOT_FOUND.asException());
-            }
-        }
-    }
-
-    private static class CellWithPolicies extends AutoScalingServiceImplBase {
-        private final Map<String, ScalingPolicyResult> policyMap;
-
-        CellWithPolicies(List<ScalingPolicyResult> policyResults) {
-            policyMap = policyResults.stream().collect(Collectors.toMap(p -> p.getId().getId(), p -> p));
-        }
-
-        @Override
-        public void getAllScalingPolicies(Empty request, StreamObserver<GetPolicyResult> responseObserver) {
-            sendScalingPolicyResults(new ArrayList<>(policyMap.values()), responseObserver);
-        }
-
-        @Override
-        public void getJobScalingPolicies(JobId request, StreamObserver<GetPolicyResult> responseObserver) {
-            List<ScalingPolicyResult> scalingPolicyResults = policyMap.values().stream()
-                    .filter(p -> p.getJobId().equals(request.getId()))
-                    .collect(Collectors.toList());
-            sendScalingPolicyResults(scalingPolicyResults, responseObserver);
-        }
-
-        @Override
-        public void getScalingPolicy(ScalingPolicyID request, StreamObserver<GetPolicyResult> responseObserver) {
-            List<ScalingPolicyResult> result = policyMap.values().stream()
-                    .filter(p -> p.getId().getId().equals(request.getId())).collect(Collectors.toList());
-            sendScalingPolicyResults(result, responseObserver);
-        }
-
-        @Override
-        public void setAutoScalingPolicy(PutPolicyRequest request, StreamObserver<ScalingPolicyID> responseObserver) {
-            ScalingPolicyID newPolicyId = ScalingPolicyID.newBuilder().setId(UUID.randomUUID().toString()).build();
-            ScalingPolicyResult newPolicyResult = ScalingPolicyResult.newBuilder().setId(newPolicyId).setJobId(request.getJobId()).build();
-
-            policyMap.put(newPolicyId.getId(), newPolicyResult);
-            responseObserver.onNext(newPolicyId);
-            responseObserver.onCompleted();
-        }
-
-        @Override
-        public void updateAutoScalingPolicy(UpdatePolicyRequest request, StreamObserver<Empty> responseObserver) {
-            if (policyMap.containsKey(request.getPolicyId().getId())) {
-                ScalingPolicyResult currentPolicy = policyMap.get(request.getPolicyId().getId());
-                ScalingPolicyResult updatePolicy = ScalingPolicyResult.newBuilder().setId(currentPolicy.getId()).setJobId(currentPolicy.getJobId())
-                        .setScalingPolicy(request.getScalingPolicy()).build();
-                policyMap.put(currentPolicy.getId().getId(), updatePolicy);
-                responseObserver.onNext(Empty.getDefaultInstance());
-                responseObserver.onCompleted();
-            } else {
-                responseObserver.onError(new IllegalArgumentException(request.getPolicyId().getId() + " is Invalid"));
-            }
-        }
-
-        @Override
-        public void deleteAutoScalingPolicy(DeletePolicyRequest request, StreamObserver<Empty> responseObserver) {
-            if (policyMap.containsKey(request.getId().getId())) {
-                policyMap.remove(request.getId().getId());
-                responseObserver.onNext(Empty.getDefaultInstance());
-                responseObserver.onCompleted();
-            } else {
-                responseObserver.onError(new IllegalArgumentException(request.getId().getId() + " is Invalid"));
-            }
-        }
-
-        private void sendScalingPolicyResults(List<ScalingPolicyResult> results,
-                                              StreamObserver<GetPolicyResult> responseObserver) {
-            GetPolicyResult.Builder getPolicyResultBuilder = GetPolicyResult.newBuilder();
-            results.forEach(getPolicyResultBuilder::addItems);
-            responseObserver.onNext(getPolicyResultBuilder.build());
-            responseObserver.onCompleted();
-        }
-    }
-
-    private static class FailingCell extends AutoScalingServiceImplBase {
-        @Override
-        public void getAllScalingPolicies(Empty request, StreamObserver<GetPolicyResult> responseObserver) {
-            responseObserver.onError(Status.UNAVAILABLE.asRuntimeException());
-        }
     }
 }
