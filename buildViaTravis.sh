505cb6f1579f947e7efc495e386e6e714a882f4e
Rob Spieldenner <rspieldenner@netflix.com>
2018-04-18 09:30:03 -0700

    Update for bintray/mavenCentral and travis-ci.org

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -3,24 +3,24 @@
 
 if [ "$TITUS_INTEGRATION_TEST" == "true" ]; then
   echo -e "Running integration tests (PR $TRAVIS_PULL_REQUEST) => Branch [$TRAVIS_BRANCH]"
-  ./gradlew -PtitusUsername="${titusUsername}" -PtitusPassword="${titusPassword}" integrationTest --stacktrace
+  ./gradlew integrationTest --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew -PtitusUsername="${titusUsername}" -PtitusPassword="${titusPassword}" build --stacktrace
+  ./gradlew build --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -PtitusUsername="${titusUsername}" -PtitusPassword="${titusPassword}" -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" build --stacktrace
+  ./gradlew -Prelease.travisci=true build --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
   *-rc\.*)
-    ./gradlew -PtitusUsername="${titusUsername}" -PtitusPassword="${titusPassword}" -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" candidate --stacktrace
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" candidate --stacktrace
     ;;
   *)
-    ./gradlew -PtitusUsername="${titusUsername}" -PtitusPassword="${titusPassword}" -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" final --stacktrace
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" final --stacktrace
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew -PtitusUsername="${titusUsername}" -PtitusPassword="${titusPassword}" build --stacktrace
+  ./gradlew build --stacktrace
 fi
