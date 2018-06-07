7e98e102950732c75b4eb18e489fc69887ebbba2
Andrew Leung <anwleung@gmail.com>
2018-06-07 07:11:25 -0700

    Fix IDE wildcard subs

--- titus-server-federation/src/test/java/com/netflix/titus/federation/service/AggregatingLoadBalancerServiceTest.java
+++ titus-server-federation/src/test/java/com/netflix/titus/federation/service/AggregatingLoadBalancerServiceTest.java
@@ -15,7 +15,13 @@
  */
 package com.netflix.titus.federation.service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
