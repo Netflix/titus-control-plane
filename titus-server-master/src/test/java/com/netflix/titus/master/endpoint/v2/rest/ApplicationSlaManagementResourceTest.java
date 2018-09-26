872928ceca6b11a9b43e2d83a5d8cec05be22111
Tomasz Bak <tbak@netflix.com>
2018-09-26 15:20:29 -0700

    Removed unused V2 API REST representation model

--- titus-server-master/src/test/java/com/netflix/titus/master/endpoint/v2/rest/ApplicationSlaManagementResourceTest.java
+++ titus-server-master/src/test/java/com/netflix/titus/master/endpoint/v2/rest/ApplicationSlaManagementResourceTest.java
@@ -19,9 +19,9 @@ package com.netflix.titus.master.endpoint.v2.rest;
 import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.base.Preconditions;
 import com.netflix.titus.api.endpoint.v2.rest.representation.ApplicationSlaRepresentation;
-import com.netflix.titus.api.endpoint.v2.rest.representation.TypeReferences;
 import com.netflix.titus.api.model.ApplicationSLA;
 import com.netflix.titus.master.service.management.ApplicationSlaManagementService;
 import com.netflix.titus.runtime.endpoint.common.rest.JsonMessageReaderWriter;
@@ -55,6 +55,10 @@ import static org.mockito.Mockito.when;
  */
 @Category(IntegrationTest.class)
 public class ApplicationSlaManagementResourceTest {
+
+    private static final TypeReference<List<ApplicationSlaRepresentation>> APPLICATION_SLA_REP_LIST_TREF =
+            new TypeReference<List<ApplicationSlaRepresentation>>() {
+            };
 
     private static final ApplicationSLA SAMPLE_SLA = ApplicationSlaSample.CriticalSmall.build();
     private static final ApplicationSlaRepresentation SAMPLE_SLA_REPRESENTATION = representationOf(SAMPLE_SLA);
@@ -109,7 +113,7 @@ public class ApplicationSlaManagementResourceTest {
                 ApplicationSlaSample.CriticalSmall.build(), ApplicationSlaSample.CriticalLarge.build()
         ));
 
-        List<ApplicationSlaRepresentation> result = client.doGET(TypeReferences.APPLICATION_SLA_REP_LIST_TREF);
+        List<ApplicationSlaRepresentation> result = client.doGET(APPLICATION_SLA_REP_LIST_TREF);
         assertThat(result).hasSize(2);
 
         verify(capacityManagementService, times(1)).getApplicationSLAs();
