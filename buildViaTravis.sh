8a3986e1cb69d6cfbf2397eb824e573e4c0bf01f
Fabio Kung <fkung@netflix.com>
2018-10-15 16:41:22 -0700

    move docker-compose bits into the build script

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 # This script will build the project.
 
-if [ "$TITUS_INTEGRATION_TEST" == "true" ]; then
+if [ "$TITUS_DOCKER_COMPOSE" == "true" ]; then
+  echo -e "Running docker-compose (PR $TRAVIS_PULL_REQUEST) => Branch [$TRAVIS_BRANCH]"
+  set -eux
+  docker-compose build
+  docker-compose -d up
+
+elif [ "$TITUS_INTEGRATION_TEST" == "true" ]; then
   echo -e "Running integration tests (PR $TRAVIS_PULL_REQUEST) => Branch [$TRAVIS_BRANCH]"
   ./gradlew integrationTest --stacktrace
+
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew build --stacktrace
+
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.travisci=true build --stacktrace
+
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
@@ -20,6 +29,7 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" final --stacktrace
     ;;
   esac
+
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   ./gradlew build --stacktrace
