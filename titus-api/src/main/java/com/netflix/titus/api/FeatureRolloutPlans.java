f1908b2303d0de41836698d16d0f4cb3ddeff33d
Tomasz Bak <tbak@netflix.com>
2019-01-08 13:37:08 -0800

    Code review update

--- titus-api/src/main/java/com/netflix/titus/api/FeatureRolloutPlans.java
+++ titus-api/src/main/java/com/netflix/titus/api/FeatureRolloutPlans.java
@@ -38,7 +38,7 @@ public interface FeatureRolloutPlans {
             deadline = "03/30/2019",
             description = "Jobs with entry point binaries containing spaces are likely relying on the legacy shell parsing " +
                     "being done by titus-executor, and are submitting entry points as a flat string, instead of breaking it into a list of arguments. " +
-                    "Jobs that have a command set will fall on the new code path that does not do any shell parsing, and do not need to be checked."
+                    "Jobs that have a command set will fall on the new code path that does not do any shell parsing, and does not need to be checked."
     )
     String ENTRY_POINT_STRICT_VALIDATION_FEATURE = "entryPointStrictValidation";
 
