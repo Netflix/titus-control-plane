a5a80cd4afba0e03165637a28906948ffcdf0cc3
tbak <tbak@netflix.com>
2019-01-10 08:44:09 -0800

    Fix flaky tests (#456)

--- titus-server-master/src/test/java/com/netflix/titus/master/integration/v3/job/JobRetryTest.java
+++ titus-server-master/src/test/java/com/netflix/titus/master/integration/v3/job/JobRetryTest.java
@@ -31,8 +31,8 @@ import com.netflix.titus.master.integration.v3.scenario.ScenarioTemplates;
 import com.netflix.titus.master.integration.v3.scenario.TaskScenarioBuilder;
 import com.netflix.titus.testkit.junit.category.IntegrationTest;
 import com.netflix.titus.testkit.junit.master.TitusStackResource;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.RuleChain;
@@ -63,21 +63,21 @@ public class JobRetryTest extends BaseIntegrationTest {
             )
             .build();
 
-    private static final TitusStackResource titusStackResource = new TitusStackResource(basicCell(5));
+    private final TitusStackResource titusStackResource = new TitusStackResource(basicCell(5));
 
-    private static final JobsScenarioBuilder jobsScenarioBuilder = new JobsScenarioBuilder(titusStackResource);
+    private final JobsScenarioBuilder jobsScenarioBuilder = new JobsScenarioBuilder(titusStackResource);
 
-    private static final InstanceGroupsScenarioBuilder instanceGroupsScenarioBuilder = new InstanceGroupsScenarioBuilder(titusStackResource);
+    private final InstanceGroupsScenarioBuilder instanceGroupsScenarioBuilder = new InstanceGroupsScenarioBuilder(titusStackResource);
 
-    @ClassRule
-    public static final RuleChain ruleChain = RuleChain.outerRule(titusStackResource).around(instanceGroupsScenarioBuilder).around(jobsScenarioBuilder);
+    @Rule
+    public final RuleChain ruleChain = RuleChain.outerRule(titusStackResource).around(instanceGroupsScenarioBuilder).around(jobsScenarioBuilder);
 
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() {
         instanceGroupsScenarioBuilder.synchronizeWithCloud().template(InstanceGroupScenarioTemplates.basicCloudActivation());
     }
 
-    @Test(timeout = 30_000)
+    @Test(timeout = LONG_TEST_TIMEOUT_MS)
     public void testBatchJobRetry() throws Exception {
         JobDescriptor<BatchJobExt> jobDescriptor = ONE_TASK_BATCH_JOB.toBuilder().withApplicationName("testBatchJobRetry").build();
         jobsScenarioBuilder.schedule(jobDescriptor, jobScenarioBuilder -> jobScenarioBuilder
@@ -93,7 +93,7 @@ public class JobRetryTest extends BaseIntegrationTest {
         );
     }
 
-    @Test(timeout = 30_000)
+    @Test(timeout = LONG_TEST_TIMEOUT_MS)
     public void testServiceJobRetry() throws Exception {
         JobDescriptor<ServiceJobExt> jobDescriptor = ONE_TASK_SERVICE_JOB.toBuilder().withApplicationName("testServiceJobRetry").build();
         jobsScenarioBuilder.schedule(jobDescriptor, jobScenarioBuilder -> jobScenarioBuilder
@@ -109,7 +109,7 @@ public class JobRetryTest extends BaseIntegrationTest {
         );
     }
 
-    @Test(timeout = 30_000)
+    @Test(timeout = LONG_TEST_TIMEOUT_MS)
     public void testBatchJobFailsAfterRetrying() throws Exception {
         JobDescriptor<BatchJobExt> jobDescriptor = ONE_TASK_BATCH_JOB.toBuilder().withApplicationName("testBatchJobFailsAfterRetrying").build();
         jobsScenarioBuilder.schedule(jobDescriptor, jobScenarioBuilder -> jobScenarioBuilder
@@ -123,7 +123,7 @@ public class JobRetryTest extends BaseIntegrationTest {
         );
     }
 
-    @Test(timeout = 30_000)
+    @Test(timeout = LONG_TEST_TIMEOUT_MS)
     public void testServiceJobFailsAfterRetrying() throws Exception {
         JobDescriptor<ServiceJobExt> jobDescriptor = ONE_TASK_SERVICE_JOB.toBuilder().withApplicationName("testServiceJobFailsAfterRetrying").build();
         jobsScenarioBuilder.schedule(jobDescriptor, jobScenarioBuilder -> jobScenarioBuilder
