60fd45cd704283961a300f574c185e0de211a03e
Fabio Kung <fabio.kung@gmail.com>
2019-04-12 10:40:51 -0700

    basic circle ci config to run all tests (#544)
    * basic circle ci config to run all tests
    
    * tune the build cache on Travis
    
    according to docs at https://docs.travis-ci.com/user/languages/java/#caching

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -3,7 +3,7 @@
 
 if [ "$TITUS_INTEGRATION_TEST" == "true" ]; then
   echo -e "Running integration tests (PR=$TRAVIS_PULL_REQUEST) => Branch [$TRAVIS_BRANCH]"
-  ./gradlew testAll --stacktrace
+  ./gradlew testAll --parallel --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew build --stacktrace
