265cf1383b9404f4414397a0b72655e443095856
Amit Joshi <ajoshi@netflix.com>
2019-07-23 13:37:44 -0700

    renaming classes

--- titus-ext/aws/src/main/java/com/netflix/titus/ext/aws/apigateway/AwsGatewayCallbackService.java
+++
@@ -1,9 +0,0 @@
-package com.netflix.titus.ext.aws.apigateway;
-
-import rx.Observable;
-
-public interface AwsGatewayCallbackService {
-    Observable<ScalingPayload> getJobInstances(String jobId);
-
-    Observable<ScalingPayload> setJobInstances(String jobId, ScalingPayload scalingPayload);
-}
--- titus-ext/aws/src/main/java/com/netflix/titus/ext/aws/apigateway/DefaultApiGatewayCallbackService.java
+++ titus-ext/aws/src/main/java/com/netflix/titus/ext/aws/apigateway/DefaultApiGatewayCallbackService.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.titus.ext.aws.apigateway;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import com.google.protobuf.UInt32Value;
 import com.netflix.titus.api.jobmanager.service.JobManagerException;
@@ -19,12 +37,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
 
-public class DefaultAwsGatewayCallbackService implements AwsGatewayCallbackService {
-    private static final Logger logger = LoggerFactory.getLogger(DefaultAwsGatewayCallbackService.class);
+@Singleton
+public class DefaultApiGatewayCallbackService implements ApiGatewayCallbackService {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultApiGatewayCallbackService.class);
     private JobServiceGateway jobServiceGateway;
     private static final String DIMENSION_NAME = "custom-resource:ResourceType:Property";
 
-    public DefaultAwsGatewayCallbackService(JobServiceGateway jobServiceGateway) {
+    @Inject
+    public DefaultApiGatewayCallbackService(JobServiceGateway jobServiceGateway) {
         this.jobServiceGateway = jobServiceGateway;
     }
 
