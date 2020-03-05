ea9dfe2c60b2f03ad11b83fba31ff2a1eb43d3e3
tbak <tbak@netflix.com>
2020-03-05 10:48:33 -0800

    HLB setup (#802)

---
+++ thlb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+HLB_SCRIPT=${PROJECT_ROOT}/build/titus.hlb
+mkdir -p ${PROJECT_ROOT}/build && rm ${HLB_SCRIPT}
+
+# HLB does not support environment variables yet (see https://github.com/openllb/hlb/issues/2)
+sed -e "s/@{{USER}}/${USER}/g" ${PROJECT_ROOT}/scripts/hlb/titus.hlb.template > ${HLB_SCRIPT}
+
+hlb run --target titusBuild ${HLB_SCRIPT}
