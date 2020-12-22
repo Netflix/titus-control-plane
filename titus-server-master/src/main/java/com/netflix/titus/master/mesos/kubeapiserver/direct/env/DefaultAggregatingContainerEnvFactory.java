05cb51d33969474951930c0184b66741df6dd3ed
Keyur Joshi <kjoshi@netflix.com>
2020-12-22 11:50:13 -0800

    Addressing code review comments

--- titus-server-master/src/main/java/com/netflix/titus/master/mesos/kubeapiserver/direct/env/DefaultAggregatingContainerEnvFactory.java
+++ titus-server-master/src/main/java/com/netflix/titus/master/mesos/kubeapiserver/direct/env/DefaultAggregatingContainerEnvFactory.java
@@ -17,18 +17,27 @@
 package com.netflix.titus.master.mesos.kubeapiserver.direct.env;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.google.common.collect.ImmutableList;
 import com.netflix.spectator.api.Id;
 import com.netflix.spectator.api.Registry;
 import com.netflix.titus.api.jobmanager.model.job.Job;
 import com.netflix.titus.api.jobmanager.model.job.Task;
 import com.netflix.titus.common.runtime.TitusRuntime;
 
+/**
+ * Default implementation aggregating container environment variables from multiple sources.
+ * Evaluation happens from left to right, with the next item overriding entries from
+ * previous evaluations if there is a collision.
+ * This class aggregates {@link UserProvidedContainerEnvFactory} and {@link TitusProvidedContainerEnvFactory}
+ * in that order.
+ */
 @Singleton
-public class DefaultAggregatingContainerEnvFactory extends AggregatingContainerEnvFactory {
+public class DefaultAggregatingContainerEnvFactory implements ContainerEnvFactory {
 
     private static final String CONFLICT_COUNTER = "titus.aggregatingContainerEnv.conflict";
 
@@ -36,9 +45,11 @@ public class DefaultAggregatingContainerEnvFactory extends AggregatingContainerE
 
     private final Id conflictId;
 
+    private final List<ContainerEnvFactory> orderedFactoryList;
+
     @Inject
     public DefaultAggregatingContainerEnvFactory(TitusRuntime titusRuntime) {
-        super(UserProvidedContainerEnvFactory.getInstance(), TitusProvidedContainerEnvFactory.getInstance());
+        orderedFactoryList = ImmutableList.of(UserProvidedContainerEnvFactory.getInstance(), TitusProvidedContainerEnvFactory.getInstance());
         this.registry = titusRuntime.getRegistry();
         this.conflictId = registry.createId(CONFLICT_COUNTER);
     }
@@ -46,7 +57,7 @@ public class DefaultAggregatingContainerEnvFactory extends AggregatingContainerE
     @Override
     public Map<String, String> buildContainerEnv(Job<?> job, Task task) {
         Map<String, String> env = new HashMap<>();
-        for (ContainerEnvFactory factory : factories) {
+        for (ContainerEnvFactory factory : orderedFactoryList) {
             Map<String, String> envMap = factory.buildContainerEnv(job, task);
             // Tracking conflicting env var for any two given factories
             env.keySet().stream().filter(envMap::containsKey).forEach(this::incrementConflictCounter);
@@ -56,6 +67,6 @@ public class DefaultAggregatingContainerEnvFactory extends AggregatingContainerE
     }
 
     private void incrementConflictCounter(String envVarName) {
-        this.registry.counter(this.conflictId.withTags("env", envVarName)).increment();
+        registry.counter(this.conflictId.withTags("env", envVarName)).increment();
     }
 }
