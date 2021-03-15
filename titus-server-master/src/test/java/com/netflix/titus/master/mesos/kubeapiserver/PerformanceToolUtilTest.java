9445a76fcc9c66f2ab7f11759a702234a12fc8d2
Tomasz Bak <tbak@netflix.com>
2021-03-15 11:03:25 -0700

    Pass mockVK rules as Titus job parameters

--- titus-server-master/src/test/java/com/netflix/titus/master/mesos/kubeapiserver/PerformanceToolUtilTest.java
+++ titus-server-master/src/test/java/com/netflix/titus/master/mesos/kubeapiserver/PerformanceToolUtilTest.java
@@ -16,19 +16,22 @@
 
 package com.netflix.titus.master.mesos.kubeapiserver;
 
-import java.time.Duration;
 import java.util.Map;
 
+import com.netflix.titus.api.jobmanager.model.job.Job;
+import com.netflix.titus.api.jobmanager.model.job.ext.BatchJobExt;
+import com.netflix.titus.testkit.model.job.JobGenerator;
 import io.titanframework.messages.TitanProtos;
 import org.apache.mesos.Protos;
 import org.junit.Test;
 
+import static com.netflix.titus.api.jobmanager.model.job.JobFunctions.appendJobDescriptorAttribute;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PerformanceToolUtilTest {
 
     @Test
-    public void testParser() {
+    public void testLegacyParser() {
         Protos.TaskInfo taskInfo = newTaskInfo(
                 "TASK_LIFECYCLE_1",
                 "selector: slots=0.. slotStep=2; launched: delay=2s; startInitiated: delay=3s; started: delay=60s; killInitiated: delay=5s"
@@ -37,6 +40,18 @@ public class PerformanceToolUtilTest {
         assertThat(annotations).containsEntry(PerformanceToolUtil.PREPARE_TIME, "3s");
         assertThat(annotations).containsEntry(PerformanceToolUtil.RUN_TIME, "60s");
         assertThat(annotations).containsEntry(PerformanceToolUtil.KILL_TIME, "5s");
+    }
+
+    @Test
+    public void testMockVK() {
+        Job<BatchJobExt> job = JobGenerator.oneBatchJob();
+        job = appendJobDescriptorAttribute(job, PerformanceToolUtil.MOCK_VK_PROPERTY_PREPARE_TIME, "10s");
+        job = appendJobDescriptorAttribute(job, PerformanceToolUtil.MOCK_VK_PROPERTY_RUN_TIME, "20s");
+        job = appendJobDescriptorAttribute(job, PerformanceToolUtil.MOCK_VK_PROPERTY_KILL_TIME, "30s");
+        Map<String, String> annotations = PerformanceToolUtil.toAnnotations(job);
+        assertThat(annotations).containsEntry(PerformanceToolUtil.PREPARE_TIME, "10s");
+        assertThat(annotations).containsEntry(PerformanceToolUtil.RUN_TIME, "20s");
+        assertThat(annotations).containsEntry(PerformanceToolUtil.KILL_TIME, "30s");
     }
 
     private Protos.TaskInfo newTaskInfo(String key, String value) {
