8283711e94ab1bedef2a8877e2e2dcd16cc44b87
Tomasz Bak <tbak@netflix.com>
2022-01-19 14:00:58 -0800

    Remove unused cluster operations and scheduler properties; remove the legacy job-activity-history modules

--- titus-testkit/src/main/java/com/netflix/titus/testkit/embedded/cell/master/EmbeddedTitusMasters.java
+++ titus-testkit/src/main/java/com/netflix/titus/testkit/embedded/cell/master/EmbeddedTitusMasters.java
@@ -30,32 +30,18 @@ public final class EmbeddedTitusMasters {
     public static EmbeddedTitusMaster basicMasterWithKubeIntegration(EmbeddedKubeCluster embeddedKubeCluster) {
         return EmbeddedTitusMaster.aTitusMaster()
                 .withEmbeddedKubeCluster(embeddedKubeCluster)
-                .withProperty("titus.agent.cacheRefreshIntervalMs", "500")
-                .withProperty("titus.agent.fullCacheRefreshIntervalMs", "500")
-                .withProperty("titus.agent.synchronizeWithInstanceCacheIntervalMs", "500")
                 .withProperty("titus.master.capacityManagement.availableCapacityUpdateIntervalMs", "10")
 
-                // Disable launch guard
-                .withProperty("titus.scheduler.globalTaskLaunchingConstraintEvaluatorEnabled", "false")
-                .withProperty("titus.scheduler.maxLaunchingTasksPerMachine", "96")
-
-                // speed up scheduling during tests
-                .withProperty("titus.scheduler.schedulerIterationIntervalMs", "1")
-                .withProperty("titus.scheduler.schedulerMaxIdleIntervalMs", "5")
-
-                .withProperty("titus.scheduler.tierSlaUpdateIntervalMs", "10")
                 .withProperty("titus.master.grpcServer.shutdownTimeoutMs", "0")
                 .withProperty("titusMaster.jobManager.taskInLaunchedStateTimeoutMs", "30000")
                 .withProperty("titusMaster.jobManager.batchTaskInStartInitiatedStateTimeoutMs", "30000")
                 .withProperty("titusMaster.jobManager.serviceTaskInStartInitiatedStateTimeoutMs", "30000")
-                .withProperty("titus.master.taskMigration.schedulerDelayInMillis", "100")
                 .withProperty("titusMaster.jobManager.reconcilerIdleTimeoutMs", "100")
                 .withProperty("titusMaster.jobManager.reconcilerActiveTimeoutMs", "10")
                 .withProperty("titus.master.grpcServer.loadbalancer.enabled", "true")
                 .withProperty("titus.master.loadBalancer.engineEnabled", "true")
                 .withProperty("titusMaster.eviction.eventStreamQuotaUpdateIntervalMs", "100")
                 .withProperty("titus.features.jobManager.disruptionBudget.whiteList", ".*")
-                .withProperty("titus.feature.kubeSchedulerEnabled", "true")
                 .withProperty("titusMaster.kubernetes.pod.gpuResourcePoolNames", EmbeddedKubeClusters.RESOURCE_POOL_GPU)
                 .withProperty("titusMaster.directKube.ebsVolumePvEnabled", "true")
                 .build();
